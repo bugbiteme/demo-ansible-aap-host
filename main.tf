8f5646468c0665fc552a2ea47ea262fc534ad9ed
bugbiteme <leon.s.levy@gmail.com>
2023-04-13 11:44:32 -0700

    added SG for webhook demo

--- main.tf
+++ main.tf
@@ -61,6 +61,13 @@ resource "aws_security_group" "sg_aap" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
